f66bf207c1b6f4d5a6b72ef26a1a31e069c051b5
Inzamam Malik <malikasinger1@users.noreply.github.com>
2018-10-28 19:20:43 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
 #install curl
 sudo apt install curl
 
+
+# instalng chrome
+sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+# add repo
+sudo echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update 
+sudo apt-get install google-chrome-stable
+
+
 #install node and npm LTS version
 curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
 sudo apt install nodejs
